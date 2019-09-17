876ef7152af1c90362ebeb198ce0aa044a91ef32
Volalex2000 <55141231+Volalex2000@users.noreply.github.com>
2019-09-17 12:00:06 +0300

    Update task_19.py

--- lab1/task_19.py
+++ lab1/task_19.py
@@ -9,26 +9,21 @@ def f1():
 def f2():
     while not wall_is_on_the_left():
         move_left(n=1)
+        
+def go():
+    if not wall_is_above() and not wall_is_beneath():
+            f1()
+            f2()
+            break
     
 @task
 def task_8_29():
-    i=0
-    if not wall_is_above() and not wall_is_beneath():
-            i=1
-    while not wall_is_on_the_left() and i==0:
+    while not wall_is_on_the_left():
+        go()
         move_left(n=1)
-        if not wall_is_above() and not wall_is_beneath():
-            i=1
-            break
-            
-    while not wall_is_on_the_right() and i==0:
+    while not wall_is_on_the_right():
+        go()
         move_right(n=1)
-        if not wall_is_above() and not wall_is_beneath():
-            i=1
-            break
-    if i==1:
-        f1()
-        f2()
         
 if __name__ == '__main__':
     run_tasks()
