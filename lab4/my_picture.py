58dee5cc4efe8da31c5194d31107aef11294f444
Volalex2000 <Volkov.AD@phistech.edu>
2019-09-23 18:54:53 +0300

    [rejected]        master -> master (fetch first) error: failed to push some refs to 'https://github.com/Volalex2000/volkov-alexandr' hint: Updates were rejected because the remote contains work that you do hint: not have locally. This is usually caused by another repository pushing hint: to the same ref. You may want to first integrate the remote changes hint: (e.g., 'git pull ...') before pushing again. hint: See the 'Note about fast-forwards' in 'git push --help' for details.

---
+++ lab4/my_picture.py
@@ -0,0 +1,39 @@
+import graphics as gr
+w = gr.GraphWin("My picture", 1000, 1000)
+
+z=gr.Rectangle(gr.Point(0, 500), gr.Point(1000, 1000))
+z.setFill('green')
+z.draw(w)
+for i in range(50,1000,50):
+    tr=gr.Line(gr.Point(i, 400), gr.Point(i+50, 500))
+    tr.setOutline('green')
+    tr.draw(w)
+for i in range(100,1000,200):
+    for j in range (600,1000,200):
+        tsl1=gr.Circle(gr.Point(i-30, j-30), 50)
+        tsl1.setFill('white')
+        tsl1.draw(w)
+        tsl2=gr.Circle(gr.Point(i-30, j+30), 50)
+        tsl2.setFill('white')
+        tsl2.draw(w)
+        tsl3=gr.Circle(gr.Point(i+30, j-30), 50)
+        tsl3.setFill('white')
+        tsl3.draw(w)
+        tsl4=gr.Circle(gr.Point(i+30, j+30), 50)
+        tsl4.setFill('white')
+        tsl4.draw(w)
+        tsc=gr.Circle(gr.Point(i, j), 50)
+        tsl4.setFill('yellow')
+        tsc.draw(w)
+d1=gr.Rectangle(gr.Point(300, 400), gr.Point(400, 800))
+d1.setFill('brown') 
+d1.draw(w)
+for i in range(200,600,100):
+    for j in range (100,500,100):
+        d2=gr.Circle(gr.Point(i, j), 150)
+        d2.setFill("#00a0a0")
+        d2.draw(w)   
+chl=Polygon(gr.Point(650, 300),gr.Point(750, 400),gr.Point(650, 300))
+chl.setWidth(30)
+w.getMouse()
+w.close()
