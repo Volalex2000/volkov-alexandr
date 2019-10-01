4c7f6861dd5c5b9ac73d671db2db5c5a26a7066a
Volalex2000 <Volkov.AD@phistech.edu>
2019-10-01 11:09:38 +0300

    6

--- lab3_2/gravitation_force.py
+++ lab3_2/gravitation_force.py
@@ -1,7 +1,15 @@
 import graphics as gr
-SIZE_X = 400
-SIZE_Y = 400
+SIZE_X = 800
+SIZE_Y = 800
 window = gr.GraphWin("Model", SIZE_X, SIZE_Y)
+
+def draw_objects(x1, y1, x2, y2, x3, y3):
+    object_1 = gr.Circle(gr.Point(x1, y1), 10)
+    object_2 = gr.Circle(gr.Point(x2, y2), 10)
+    object_3 = gr.Circle(gr.Point(x3, y3), 10)
+    object_1.draw(window)
+    object_2.draw(window)
+    object_3.draw(window)
 
 def change_coordinates():
     global x1, y1, x2, y2, x3, y3, vx1, vy1, vx2, vy2, vx3, vy3
@@ -12,23 +20,32 @@ def change_coordinates():
     x3 = x3 + vx3
     y3 = y3 + vy3
 
+def change_speed():
+    global x1, y1, x2, y2, x3, y3, vx1, vy1, vx2, vy2, vx3, vy3
+    vx1 = vx1 + force_x_all(x1, y1, x2, y2, x3, y3)
+    vy1 = vy1 + force_y_all(x1, y1, x2, y2, x3, y3)
+    vx2 = vx2 + force_x_all(x2, y2, x3, y3, x1, y1)
+    vy2 = vy2 + force_y_all(x2, y2, x3, y3, x1, y1)
+    vx3 = vx3 + force_x_all(x3, y3, x1, y1, x2, y2)
+    vy3 = vy3 + force_y_all(x3, y3, x1, y1, x2, y2)
+
+def force_x_all(x1, y1, x2, y2, x3, y3):
+    return (force_x_2_objects(x1, y1, x2, y2) + force_x_2_objects(x1, y1, x3, y3))
+
+def force_y_all(x1, y1, x2, y2, x3, y3):
+    return (force_y_2_objects(x1, y1, x2, y2) + force_y_2_objects(x1, y1, x3, y3))
+
 def force_x_2_objects(x1, y1, x2, y2):
-    if module_f(x1, x2) != 0:
-        return (10000 / distance_objects(x1, y1, x2, y2)) * ((1 + (((y2 - y1) / (x2 - x1)) ** 2)) ** -0.5)*module_f(x1, x2)
+    if x1 != x2:
+        return (10000 / distance_objects(x1, y1, x2, y2)) * ((1 + (((y2 - y1) / (x2 - x1)) ** 2)) ** -0.5) * module_f(x1, x2)
     else:
         return 0
 
 def force_y_2_objects(x1, y1, x2, y2):
-    if module_f(y1, y2) != 0:
-        return (10000 / distance_objects(x1, y1, x2, y2)) * ((1 + (((x2 - x1) / (y2 - y1)) ** 2)) ** -0.5)*module_f(y1, y2)
+    if y1 != y2:
+        return (10000 / distance_objects(x1, y1, x2, y2)) * ((1 + (((x2 - x1) / (y2 - y1)) ** 2)) ** -0.5) * module_f(y1, y2)
     else:
         return 0
-
-def force_x_all(x1, y1, x2, y2, x3, y3):
-    return (force_y_2_objects(x1, y1, x2, y2) + force_y_2_objects(x1, y1, x3, y3))
-
-def force_y_all(x1, y1, x2, y2, x3, y3):
-    return (force_y_2_objects(x1, y1, x2, y2) + force_y_2_objects(x1, y1, x3, y3))
 
 def distance_objects(x1, y1, x2, y2):
     return (x1-x2)**2+(y1-y2)**2
@@ -39,42 +56,36 @@ def module_f(x1, x2):
     else:
         return 0
 
-def change_speed():
-    global x1, y1, x2, y2, x3, y3, vx1, vy1, vx2, vy2, vx3, vy3
-    vx1 = vx1 + force_x_all(x1, y1, x2, y2, x3, y3)
-    vy1 = vy1 + force_y_all(x1, y1, x2, y2, x3, y3)
-    vx2 = vx2 + force_x_all(x2, y2, x3, y3, x1, y1)
-    vy2 = vy2 + force_y_all(x2, y2, x3, y3, x1, y1)
-    vx3 = vx3 + force_x_all(x3, y3, x1, y1, x2, y2)
-    vy3 = vy3 + force_y_all(x3, y3, x1, y1, x2, y2)
 
-def draw_objects(x1, y1, x2, y2, x3, y3):
-    object_1 = gr.Circle(gr.Point(x1, y1), 10)
-    object_2 = gr.Circle(gr.Point(x2, y2), 10)
-    object_3 = gr.Circle(gr.Point(x3, y3), 10)
-    object_1.draw(window)
-    object_2.draw(window)
-    object_3.draw(window)
-
-x1 = 100
-y1 = 200
+x1 = 200
+y1 = 300
 x2 = 200
 y2 = 200
-x3 = 300
-y3 = 200
-draw_objects(x1, y1, x2, y2, x3, y3)
+x3 = 200
+y3 = 100
 
-vx1 = 0
+object_1 = gr.Circle(gr.Point(x1, y1), 10)
+object_2 = gr.Circle(gr.Point(x2, y2), 10)
+object_3 = gr.Circle(gr.Point(x3, y3), 10)
+object_1.draw(window)
+object_2.draw(window)
+object_3.draw(window)
+
+vx1 = 5
 vy1 = 0
 vx2 = 0
 vy2 = 0
-vx3 = 0
+vx3 = -5
 vy3 = 0
 
 for i in range(30):
     change_coordinates()
-    draw_objects(x1, y1, x2, y2, x3, y3)
+    print(force_x_all(x1, y1, x2, y2, x3, y3))
+    object_1.move(vx1, vy1)
+    object_2.move(vx2, vy2)
+    object_3.move(vx3, vy3)
     change_speed()
+    gr.time.sleep(0.1)
     
 window.getMouse()
 window.close()
