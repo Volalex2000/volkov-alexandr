8ab16193b8850185bcd32823a56191756648feaa
Volalex2000 <Volkov.AD@phistech.edu>
2019-10-05 20:24:31 +0300

    2 Работает, делаю функцию main2 Работает, делаю функцию main

--- lab3_2/gravitation_force.py
+++ lab3_2/gravitation_force.py
@@ -11,7 +11,7 @@ def draw_objects(coord_1, coord_2, coord_3):
     object_2.draw(window)
     object_3.draw(window)
 
-def change_coordinates(coordinate, speed):
+def change_coordinates(coord_1, coord_2, coord_3, speed_1, speed_2, speed_3):
     for i in range(2):
         coord_1[i] = coord_1[i] + speed_1[i]
         coord_2[i] = coord_2[i] + speed_2[i]
@@ -33,13 +33,13 @@ def force_y_all(c1, c2, c3):
 
 def force_x_2_objects(c1, c2):
     if c1[0] != c2[0]:
-        return (3500 / distance_objects(x1, y1, x2, y2)) * ((1 + (((c2[1] - c1[1]) / (c2[0] - c1[0])) ** 2)) ** -0.5) * module_f(c1[0], c2[0])
+        return (3500 / distance_objects(c1, c2)) * ((1 + (((c2[1] - c1[1]) / (c2[0] - c1[0])) ** 2)) ** -0.5) * module_f(c1[0], c2[0])
     else:
         return 0
 
 def force_y_2_objects(c1, c2):
     if c1[1] != c2[1]:
-        return (3500 / distance_objects(x1, y1, x2, y2)) * ((1 + (((c2[0] - c1[0]) / (c2[1] - c1[1])) ** 2)) ** -0.5) * module_f(c1[1], c2[1])
+        return (3500 / distance_objects(c1, c2)) * ((1 + (((c2[0] - c1[0]) / (c2[1] - c1[1])) ** 2)) ** -0.5) * module_f(c1[1], c2[1])
     else:
         return 0
 
@@ -47,7 +47,7 @@ def distance_objects(c1, c2):
     return (c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2
 
 def module_f(d1, d2):
-    if x1 != x2: 
+    if d1 != d2: 
         return (d2- d1) / abs(d2 - d1)
     else:
         return 0
@@ -55,12 +55,6 @@ def module_f(d1, d2):
 coord_1 = [400, 400]
 coord_2 = [300, 300]
 coord_3 = [200, 200]
-#coordinate[1] = 400 #x1 = 400
-#coordinate[2] = 400 #y1 = 400
-#coordinate[3] = 300 #x2 = 300
-#coordinate[4] = 300 #y2 = 300
-#coordinate[5] = 200 #x3 = 200
-#coordinate[6] = 200 #y3 = 200
 
 object_1 = gr.Circle(gr.Point(coord_1[0], coord_1[1]), 10)
 object_2 = gr.Circle(gr.Point(coord_2[0], coord_2[1]), 10)
@@ -72,28 +66,22 @@ object_3.draw(window)
 speed_1 = [-4, 2]
 speed_2 = [0, 0]
 speed_3 = [4, -2]
-#speed[1] = -4 vx1 = -4
-#speed[2] = 2 vy1 = 2
-#speed[3] = 0 vx2 = 0
-#speed[4] = 0 vy2 = 0
-#speed[5] = 4 vx3 = 4
-#speed[6] = -2 vy3 = -2
 
 for i in range(1500):
-    change_coordinates()
+    change_coordinates(coord_1, coord_2, coord_3, speed_1, speed_2, speed_3)
 
-    object_1.move(speed_1)
-    object_2.move(speed_2)
-    object_3.move(speed_3)
+    object_1.move(speed_1[0], speed_1[1])
+    object_2.move(speed_2[0], speed_2[1])
+    object_3.move(speed_3[0], speed_3[1])
 
-    drow_coordinates_1 = gr.Point(coord_1)
-    drow_coordinates_2 = gr.Point(coord_2)
-    drow_coordinates_3 = gr.Point(coord_3)
+    drow_coordinates_1 = gr.Point(coord_1[0], coord_1[1])
+    drow_coordinates_2 = gr.Point(coord_2[0], coord_2[1])
+    drow_coordinates_3 = gr.Point(coord_3[0], coord_3[1])
     drow_coordinates_1.draw(window)
     drow_coordinates_2.draw(window)
     drow_coordinates_3.draw(window)
 
-    change_speed()
+    change_speed(coord_1, coord_2, coord_3, speed_1, speed_2, speed_3)
     gr.time.sleep(0.1)
     
 window.getMouse()
