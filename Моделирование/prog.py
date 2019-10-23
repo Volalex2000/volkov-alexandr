0f6d7ca570797d020316ed0d09b75bebdb66a990
Volalex2000 <Volkov.AD@phistech.edu>
2019-10-23 17:54:42 +0300

    Запускается, скорости и координаты работают, неверное определение сил, проблемы с инициализациией частиц

---
+++ Моделирование/prog.py
@@ -0,0 +1,59 @@
+def initial_conditions():#начальные условия
+    input(time)#время процесса
+    input(number_elements)#кол-во частиц 
+
+def creat_elements():#задает начальные условия частицам
+    elems = []#список частиц и их свойства
+    for i in number_elements:#кол-во частиц
+        element = {}
+        element['x'] = 0
+        element['y'] = 0
+        element['z'] = 0
+        element['vx'] = 0
+        element['vy'] = 0
+        element['vz'] = 0
+        elems.append(element)
+        
+def main():
+    initial_conditions()
+    creat_elements()
+
+    for i in range(number_elements):#движение во времени 
+    
+def creat_elements():#задает начальные условия частицам
+    elems = []#список частиц и их свойства
+    for i in number_elements:#кол-во частиц
+        element = {}
+        element['x'] = 0
+        element['y'] = 0
+        element['z'] = 0
+        element['vx'] = 0
+        element['vy'] = 0
+        element['vz'] = 0
+        elems.append(element)
+        
+def main():
+   initial_conditions()
+    
+    initial_conditions()
+    creat_elements()
+    for iat_elements() in range(1):#движение во времени 
+
+def creat_elements():#задает начальные условия частицам
+    elems = []#список частиц и их свойства
+    for i in number_elements:#кол-во частиц
+        element = {}
+        element['x'] = 0
+        element['y'] = 0
+        element['z'] = 0
+        element['vx'] = 0
+        element['vy'] = 0
+        element['vz'] = 0
+        elems.append(element)
+        
+def main():
+   initial_conditions()
+    
+    initial_conditions()
+    creat_elements()
+    for iat_elements() in range(1):#движение во времени 
